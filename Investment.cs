c877ea6b82f940f99ca691e199f1a86255a9c0b9
Pol Torras Claverol <poltorrasclaverol@gmail.com>
2026-03-01 19:08:13 +0100

    Add files via upload

---
+++ Investment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FintechInvestment.Core
+{
+    public class Investment
+    {
+        public double total(double interes, double quantitat, int temps)
+        {
+            double total;
+            double totalinteres;
+
+            totalinteres = interes / 100;
+
+            total = quantitat;
+
+            for (int i = 0; i < temps; i++)
+            {
+                total += Math.Round(total * totalinteres, 2);
+            }
+
+            return total;
+        }
+
+        public double guany(double total, double quantitat)
+        {
+            double guany = Math.Round( total - quantitat, 2);
+            return guany;
+        }
+
+    }
+}
